d4e7db481cb9108ae13139a4871aa74ffe22037e
ethan ozelius <e.ozelius@gmail.com>
2017-07-18 17:27:12 -0400

    add a navbar that is working well and error free

--- app/javascript/packs/navbar.jsx
+++ app/javascript/packs/navbar.jsx
@@ -6,21 +6,36 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
-const NavBar = props => (
-  <div>hello {props.name} you fuckgg</div>
-)
+class NavBar extends React.Component {
+  render(){
+    return (
+      <nav className="navbar navbar-toggleable-md navbar-light navbar-inverse bg-inverse">
+        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <a className="navbar-brand" href="#">driftMaps</a>
 
-NavBar.defaultProps = {
-  name: 'David'
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="#">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
 }
 
-NavBar.propTypes = {
-  name: PropTypes.string
-}
+NavBar.defaultProps = {}
+NavBar.propTypes = {}
 
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <NavBar name="ethan" />,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
+
+
+document.addEventListener('DOMContentLoaded', ReactDOM.render(<NavBar />, document.getElementById('navbar-container')))
