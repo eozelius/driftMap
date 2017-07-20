1d85d5a8eae0d00967f83f38c1d20facfc41ff66
ethan ozelius <e.ozelius@gmail.com>
2017-07-20 02:03:30 -0400

    welcome mailer initial commit

--- spec/mailers/previews/user_preview.rb
+++ spec/mailers/previews/user_preview.rb
@@ -1,4 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user
 class UserPreview < ActionMailer::Preview
+  def welcome_to_driftmap
+    @user = User.first
+    @greeting = "Greetings, #{@user.first_name} #{@user.last_name}"
 
+    # mail to: @user.email, subject: "Welcome to driftMap"
+    UserMailer.welcome_to_driftmap(@user)
+  end
 end
